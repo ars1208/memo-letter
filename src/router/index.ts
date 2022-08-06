b436c68a292b3c9f32d33355552173327ef10768
ars1208 <ara.kaz1208@gmail.com>
2022-08-06 23:16:28 +0900

    chore: Authの状態保持処理

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import store from "@/store/index";
+import Auth from "@aws-amplify/auth";
 import LogIn from "../views/LogIn.vue";
 import Home from "../views/Home.vue";
 import Letter from "../views/Letter.vue";
@@ -28,6 +30,27 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+function getAuthenticatedUser() {
+  return Auth.currentAuthenticatedUser()
+    .then((data) => {
+      if (data && data.signInUserSession) {
+        store.commit("setUser", data);
+        return data;
+      }
+    })
+    .catch((e) => {
+      console.error(e);
+      store.commit("setUser", null);
+      return null;
+    });
+}
+
+router.beforeResolve(async (to, from, next) => {
+  await getAuthenticatedUser();
+
+  return next();
 });
 
 export default router;
