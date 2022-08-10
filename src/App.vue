3dac899abe60792f6e2040f5eb21263055849dba
ars1208 <ara.kaz1208@gmail.com>
2022-08-11 02:12:08 +0900

    chore: routeの修正

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/memo-letter">Home</router-link> |
+    <router-link to="/memo-letter/about">About</router-link>
   </nav>
   <router-view />
 </template>
